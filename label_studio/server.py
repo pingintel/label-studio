02ec50eb2772059b713abde1bcf5bdd6149b0f63
dheerajck <dheerajck18@gmail.com>
2024-05-22 19:31:54 +0530

    Moved from khan.settings.prod to khan.settings.local because we are using sqlite

--- label_studio/server.py
+++ label_studio/server.py
@@ -37,7 +37,7 @@ DEFAULT_USERNAME = 'default_user@localhost'
 
 def _setup_env():
     sys.path.insert(0, LS_PATH)
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "khan.settings.prod")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "khan.settings.local")
     application = get_wsgi_application()
 
 
