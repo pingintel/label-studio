6ce805589ec38d24769a7be105ff7d3bca84003a
b-gran <bill@billgranfield.com>
2024-08-05 15:20:45 -0400

    add path

--- label_studio/khan/apps.py
+++ label_studio/khan/apps.py
@@ -1,5 +1,8 @@
+import os
 from django.apps import AppConfig
 
 
 class KhanConfig(AppConfig):
     name = 'khan'
+    path = os.path.normpath(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'khan'))
+
